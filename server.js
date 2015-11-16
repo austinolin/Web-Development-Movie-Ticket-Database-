95944262eb0570d77a2c67a6c6fdf0495b46b24f
Austin Olin <aeolin13@gmail.com>
2015-11-15 16:27:39 -0800

    dkfajdsf

--- server.js
+++ server.js
@@ -1,9 +1,7 @@
 var express = require('express');
-var app = express();
-
-
 
 var bodyParser = require('body-parser');
+var app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
