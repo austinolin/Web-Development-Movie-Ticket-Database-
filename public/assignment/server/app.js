0a41b8ec01afbe5fceadfe02a578284102c6d179
Austin Olin <aeolin13@gmail.com>
2015-11-30 22:26:48 -0800

    added mongodb cartridge

--- public/assignment/server/app.js
+++ public/assignment/server/app.js
@@ -5,7 +5,19 @@ module.exports = function(app)
 	// var node_uuid = require('node-uuid');
 
 	var mongoose = require('mongoose');
-	mongoose.connect('mongodb://localhost/cs5610');
+	// mongoose.connect('mongodb://localhost/cs5610');
+
+	var connection = 'mongodb://127.0.0.1:27017/cs5610';
+
+	if(process.env.OPENSHIFT_MONGODB_DB_PASSWORD) {
+	    connection = process.env.OPENSHIFT_MONGODB_DB_USERNAME + ":" +
+	        process.env.OPENSHIFT_MONGODB_DB_PASSWORD + "@" +
+	        process.env.OPENSHIFT_MONGODB_DB_HOST + ':' +
+	        process.env.OPENSHIFT_MONGODB_DB_PORT + '/' +
+	        process.env.OPENSHIFT_APP_NAME;
+	}
+
+	mongoose.connect(connection);
 	var db = mongoose.connection;
 
 	// Models
